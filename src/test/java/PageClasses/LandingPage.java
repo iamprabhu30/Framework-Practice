ce163b6c045fc740838b40a5ecf1d39728e9dd0e
iamprabhu30 <97522292+iamprabhu30@users.noreply.github.com>
2024-10-06 13:01:24 +0530

    Add files via upload
    Green kart Ecommerce site product search scenario(product search, window handling, add to cart) added

---
+++ src/test/java/PageClasses/LandingPage.java
@@ -0,0 +1,34 @@
+package PageClasses;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+
+public class LandingPage {
+	
+	public WebDriver driver;
+	public LandingPage(WebDriver driver)
+	{	
+		this.driver=driver;
+		
+	}
+
+	
+	By search = By.xpath("//input[@type='search']");
+	By productName =By.cssSelector("h4.product-name");
+	By linkTopDeals =By.linkText("Top Deals");
+	
+	
+	
+	public void SearchText(String shortname)
+	{
+		driver.findElement(search).sendKeys(shortname);
+	}
+	public String getProductName()
+	{
+		return driver.findElement(productName).getText();
+	}
+	public void TopDealsLink()
+	{
+		driver.findElement(linkTopDeals).click();
+	}
+}
